18b8fa88c94f70d7a60ea61bd3c597ce9bf9f157
Chris Watnee <chris@watnee.us>
2020-10-25 06:17:25 -0400

    Add questions

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,4 +1,12 @@
 const mixedMessages = {
   greetings: ['Hi', 'Hello', 'Good morning', 'Good afernoon', 'Hey'],
-  names: ['Ross', 'Rachel', 'Monica', 'Joey', 'Chandler', 'Phoebe']
+  names: ['Ross', 'Rachel', 'Monica', 'Joey', 'Chandler', 'Phoebe'],
+  questions: [
+    'What’s your favorite book?',
+    'Do you like to cook?',
+    'What’s on your bucket list?',
+    'What do you do for a living?',
+    'Do you believe in luck?',
+    'What’s your hidden talent?'
+  ]
 };
