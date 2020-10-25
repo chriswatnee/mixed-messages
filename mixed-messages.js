6fe65f84569c25f43f0a2623edaeadd3257a200f
Chris Watnee <chris@watnee.us>
2020-10-25 06:05:43 -0400

    Add names

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,3 +1,4 @@
 const mixedMessages = {
-  greetings: ['Hi', 'Hello', 'Good morning', 'Good afernoon', 'Hey']
+  greetings: ['Hi', 'Hello', 'Good morning', 'Good afernoon', 'Hey'],
+  names: ['Ross', 'Rachel', 'Monica', 'Joey', 'Chandler', 'Phoebe']
 };
